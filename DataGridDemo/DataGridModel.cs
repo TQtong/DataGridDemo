40c736efdebd61c2e142aa8e68f9efd861032a2c
TQ <tangqi@southzn.com>
2022-08-21 18:49:10 +0800

    添加项目文件。

---
+++ DataGridDemo/DataGridModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridDemo
+{
+    public class DataGridModel : BindableObject
+    {
+		private string name;
+		/// <summary>
+		/// 名字
+		/// </summary>
+		public string Name
+		{
+			get => name;
+			set
+			{
+				name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private int number;
+		/// <summary>
+		/// 数量
+		/// </summary>
+		public int Number
+		{
+			get => number;
+			set
+			{
+				number = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private double price;
+		/// <summary>
+		/// 价格
+		/// </summary>
+		public double Price
+		{
+			get => price;
+			set
+			{
+				price = value;
+				OnPropertyChanged();
+			}
+		}
+
+	}
+}
---
+++ DataGridDemo/DataGridViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridDemo
+{
+    public class DataGridViewModel : BindableObject
+    {
+        public ObservableCollection<DataGridModel> DataGridModels { get; set; } = new ObservableCollection<DataGridModel>();
+
+        public DataGridViewModel()
+        {
+            Create();
+        }
+
+        private void Create()
+        {
+            DataGridModels.Add(new DataGridModel()
+            {
+                Name = "苹果",
+                Number = 5,
+                Price = 1.5
+            });
+            DataGridModels.Add(new DataGridModel()
+            {
+                Name = "香蕉",
+                Number = 2,
+                Price = 15
+            });
+            DataGridModels.Add(new DataGridModel()
+            {
+                Name = "菠萝",
+                Number = 3,
+                Price = 10
+            });
+            DataGridModels.Add(new DataGridModel()
+            {
+                Name = "葡萄",
+                Number = 6,
+                Price = 12
+            });
+            DataGridModels.Add(new DataGridModel()
+            {
+                Name = "椰子",
+                Number = 10,
+                Price = 30
+            });
+        }
+    }
+}
